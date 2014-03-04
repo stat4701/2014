6f64fa4b73284304334f43ad3a1a42d0821b22fe
shiyawang <shiya.wang@gmail.com>
2014-03-03 19:50:26 -0500

    marchcommit

--- projects/popgraph/get-data.py
+++ projects/popgraph/get-data.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib2
+from re import sub
+import operator
 import csv
 
 class PopulationTableGrabber(object):
@@ -17,21 +19,18 @@ class PopulationTableGrabber(object):
         return self.soup.find_all(table_has_enough_rows)
 
     def parse_one_table(self, tab):
-        ''' munge the table rows
+        ''' somewhat dirty, but grabs the year, skips a row, and parses the rest
+            would break if there were and asterisk row for example
         '''
         yearElement = tab.find(colspan='3')
         year = int(yearElement.get_text())
-        rows = tab.find_all('tr')
-        print rows
+        rows = yearElement.find_parent().find_next_sibling().find_next_siblings()
         out = []
         for row in rows:
-            if row.find('td').has_attr('colspan'):
-                continue
             tds = row.find_all('td')
-            print tds
             rowdata = {
                 'year': year,
-                'rank': tds[0].get_text(), 
+                'rank': self.fix_ordinals(tds[0].get_text()), 
                 'city': tds[1].get_text(), 
                 'pop': float(tds[2].get_text())
             }
@@ -39,13 +38,21 @@ class PopulationTableGrabber(object):
         return out
 
     def reshape_city_data(self, all_tabs):
-        ''' list of lists'''
-        return [self.parse_one_table(tab) for tab in all_tabs]
+        ''' list += rows, for all the tables'''
+        return reduce(operator.add, 
+                      [self.parse_one_table(tab) for tab in all_tabs]
+        )
+
+
+    def fix_ordinals(self, string):
+        ''' oh look, "1."
+        '''
+        return int(sub('[.]', '', string))
+
 
     def write_csv(self, dicts, filename="citypop.csv"):
         ''' could do without ceremony, but this preserves key order
         '''
-        print dicts
         keys = ['year', 'rank', 'city', 'pop']
         f = open(filename, 'wb')
         dict_writer = csv.DictWriter(f, keys)
@@ -61,4 +68,3 @@ pop.write_csv(pop.reshape_city_data(pop.find_all_tabs()))
 ## https://docs.google.com/spreadsheet/pub?key=0AruyJI76uB8RdFBySUhzRldyalJGTXZtY1NxT0E2Z1E&output=csv
 ## source: http://www.census.gov/population/censusdata/table-4.pdf
 ## and census factfinder2 tables for 2000, 2010.
-
