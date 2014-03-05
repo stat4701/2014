b1aa700802a650969419c4f15069a58c70934358
Michael Malecki <mike@crunch.io>
2014-03-05 17:22:08 -0500

    data and some packaging

---
+++ projects/various-data-sources/pizza/pkg/R/pkg-internal.R
@@ -0,0 +1,9 @@
+##' pizzasizes
+##' A data.frame with the data for
+##' http://www.npr.org/blogs/money/2014/02/26/282132576/74-476-reasons-you-should-always-get-the-bigger-pizza
+##'
+##' @name pizzasizes
+##' @docType data
+##' @keywords datasets
+NULL
+
